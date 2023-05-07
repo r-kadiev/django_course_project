6caf28fd196f5d27f7490291d328411f7fe8bd6f
rizvan <rizvan@MacBook-Pro-rizvan.local>
2023-05-07 22:29:16 +0300

    create_models_category_and_migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=100)
+    slug = models.SlugField(verbose_name="URL", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
